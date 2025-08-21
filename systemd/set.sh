957effca48cb804d002baab0ea2f536703c94450
keita <keitagame0319@icloud.com>
2025-08-22 08:27:29 +0900

    Create set.sh

---
+++ systemd/set.sh
@@ -0,0 +1,24 @@
+dd if=/dev/zero of="$ISO_ROOT/efiboot.img" bs=1M count=200
+mkfs.vfat "$ISO_ROOT/efiboot.img"
+
+
+# 2. マウントしてファイルコピー
+mkdir mnt_esp
+
+
+# 2. マウントしてファイルコピー
+sudo mount "$ISO_ROOT/efiboot.img" mnt_esp
+
+mkdir -p mnt_esp/EFI/BOOT
+cp /usr/lib/systemd/boot/efi/systemd-bootx64.efi mnt_esp/EFI/BOOT/BOOTX64.EFI
+cp "$AIROOTFS/boot/vmlinuz-linux" mnt_esp/
+cp "$AIROOTFS/boot/initramfs-linux.img" mnt_esp/
+# loader.conf と arch.conf を配置
+mkdir -p mnt_esp/loader/entries
+cp ./systemd/loader.conf mnt_esp/loader/
+cp ./systemd/arch.conf mnt_esp/loader/entries/
+
+
+
+sudo umount -l mnt_esp
+rmdir mnt_esp
